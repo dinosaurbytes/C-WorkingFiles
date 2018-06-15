1882c03a482ff1ca04765f4070d40a645bb97c0f
Brian Liang <792783@ACDM.DS.SAIT.CA>
2018-06-15 14:43:47 -0600

    Read and Writes information to an external text file

---
+++ ProductsWithTextFile/ProductsWithTextFile/frmProducts.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ProductsWithTextFile
+{
+    partial class frmProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstProducts = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblNrProducts = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblNrProducts);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtPrice);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(39, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(419, 276);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "New Product";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(241, 124);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(138, 77);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(178, 20);
+            this.txtPrice.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(138, 42);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(178, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Price";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // lstProducts
+            // 
+            this.lstProducts.FormattingEnabled = true;
+            this.lstProducts.Location = new System.Drawing.Point(535, 58);
+            this.lstProducts.Name = "lstProducts";
+            this.lstProducts.Size = new System.Drawing.Size(216, 251);
+            this.lstProducts.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(532, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "My Products";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "#Products";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(190, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 6;
+            this.label5.UseWaitCursor = true;
+            // 
+            // lblNrProducts
+            // 
+            this.lblNrProducts.AutoSize = true;
+            this.lblNrProducts.Location = new System.Drawing.Point(208, 217);
+            this.lblNrProducts.Name = "lblNrProducts";
+            this.lblNrProducts.Size = new System.Drawing.Size(56, 13);
+            this.lblNrProducts.TabIndex = 7;
+            this.lblNrProducts.Text = "#Products";
+            // 
+            // frmProducts
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstProducts);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmProducts";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmProducts_FormClosing);
+            this.Load += new System.EventHandler(this.frmProducts_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstProducts;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNrProducts;
+    }
+}
+
---
+++ ProductsWithTextFile/ProductsWithTextFile/frmProducts.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductsWithTextFile
+{
+    public partial class frmProducts : Form
+    {
+
+        //Parallel Arrays
+        const string path = "products.txt"; 
+        const int MAX_SIZE = 100;
+        string[] names = new string[MAX_SIZE];
+        decimal[] prices = new decimal[MAX_SIZE];
+        int howMany = 0;        //actual number of products
+
+        public frmProducts()
+        {
+            InitializeComponent();
+        }
+
+
+
+        
+        private void frmProducts_Load(object sender, EventArgs e)
+        {
+            ReadProducts();
+            DisplayProducts();
+        }
+
+        private void ReadProducts()
+        {
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                //Open the file for reading (the very frist time, the file does not exist)
+                fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read);
+                sr = new StreamReader(fs);
+                string line;
+                string[] parts;
+
+                // read data
+                while(!sr.EndOfStream)             //while there is still data
+                {
+                    line = sr.ReadLine();
+                    parts = line.Split(',');        // split where commas are
+                    //store data in the arrays
+                    names[howMany] = parts[0];
+                    prices[howMany] = Convert.ToDecimal(parts[1]);
+                    howMany++;
+
+                }
+               // lblNrProducts.Text = howMany.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error while reading data: " + ex.Message, ex.GetType().ToString());
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+
+        }
+        // file paths cause use the back slash escape character \\ for the file path
+        // you can also use the @ symbol
+        private void SaveProducts()
+        {
+            FileStream fs = null;
+            StreamWriter sw = null;
+            string line;
+            try
+            {
+                //Open the file for writing; overwrite old content
+                fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                
+                // save data
+                for (int i=0; i < howMany; i++) //for each product
+                {
+                    line = names[i] + ", " + prices[i].ToString();
+                    sw.WriteLine(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error while reading data: " + ex.Message, ex.GetType().ToString());
+            }
+            finally
+            {
+                if (sw != null) sw.Close();     //test for null incase it crashed before it could write anything
+            }
+
+        }
+        private void DisplayProducts()
+        {
+            lstProducts.Items.Clear();        //start with empty list box
+            for(int i = 0; i<howMany; i++)  //for each product
+            {
+                lstProducts.Items.Add(names[i] + ":\t" + prices[i].ToString("c"));
+            }
+            lblNrProducts.Text = howMany.ToString();
+        }
+        private void frmProducts_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveProducts();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //get inputs and store in the arrays
+            names[howMany] = txtName.Text;
+            prices[howMany] = Convert.ToDecimal(txtPrice.Text);
+            howMany++;
+            //re-display
+            DisplayProducts();
+
+        }
+
+        //
+        
+    }
+}
