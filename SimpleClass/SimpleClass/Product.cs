eff971fea872b77ce11b142b67a2eccfbdc270c9
Brian Liang <792783@ACDM.DS.SAIT.CA>
2018-06-18 09:49:38 -0600

    Example of Classes

---
+++ SimpleClass/SimpleClass/Product.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClass
+{
+    /* 
+     * good habbit to create class as public (incase you are creating libraries)
+     * Good idea to provide a constructor. Which is a string method. Constructors do not have a return type. Constructor has same method name of the class.
+     */
+    public class Product
+    {
+        //private data (typically lowercase)
+        //private string name;
+        private decimal price;
+        private int quantity;
+
+        //AUTO-IMPLEMENTED PROPERTY (SHORTER METHOD)
+        /*
+         * Compiler creates hidden variable with no name
+         * YOu can now get ride of private data in lines 16-18
+         */
+         public string Name { get; set; }
+
+
+
+
+
+
+
+        //ACCESSOR PROPERTY (LONGER WAY)
+        /*
+         * Typically use captial for accessors 
+         */
+        //public string Name
+        //{
+        //    get
+        //    {
+        //        return name;
+        //    }
+        //    set
+        //    {
+        //        name = value; //value provided when setting, value on right hand side of assignment "big box" for example
+        //    }
+        //}
+        public decimal Price
+        {
+            get { return price; }
+            set { price = (value < 0) ? price = 0 : value;}
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = (value < 0) ? quantity = 0 : value; }
+        }
+
+        //public Product() {}
+
+        //create constructor. 
+        /*
+         * Providing default values, allows you to avoid having an empty consturctor "Product()"
+         */
+        public Product(string n = "Unknown", decimal p = 0, int q = 0)
+        {
+            Name = n;           //Use captial, same of accessors
+            Price = p;
+            Quantity = q;
+        }
+
+        //METHOD
+        public decimal InventoryValue()
+        {
+            return price * quantity;
+        }
+
+        //method
+        /*
+         * methods are typically public
+         * can define a method ToString
+         * trick needs to use override or else it creates a totally seperate method. 
+         * allows writing of the data automatically
+         */
+         public  override string ToString()
+        {
+            return Name + ": " + price.ToString("c") + " - " + quantity.ToString();
+        }
+    }
+}
---
+++ SimpleClass/SimpleClass/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calling constructor without any paramaters
+            Product prod1 = new Product();
+            Product prod2 = new Product("Gadget");
+            Product prod3 = new Product("Widget", 4.99m);
+            Product prod4 = new Product("Gizmo", 2.99m, 15);
+            Product prod5 = new Product("Dummy", -1.99m, -10);
+
+            //Create a list of products
+            List<Product> inventory = new List<Product>();      //empty list
+            inventory.Add(prod1);
+            inventory.Add(prod2);
+            inventory.Add(prod3);
+            inventory.Add(prod4);
+            inventory.Add(prod5);
+
+
+            //testing accessor properties
+            prod1.Name = "big box";     //this line uses the set part of the accessor property
+            //Console.WriteLine("The first product is " + prod1.Name);        //this line uses the get part
+            prod1.Price = -5;
+
+            //Displays on console
+            //Console.WriteLine(prod1);
+            //Console.WriteLine(prod2);
+            //Console.WriteLine(prod3);
+            //Console.WriteLine(prod4);
+            //Console.WriteLine(prod5);
+
+            //One method of displaying is using element index
+            //for (int i = 0; i < inventory.Count; i++)
+            //    Console.WriteLine(inventory[i]);        // Can access list elemetn using index
+
+            //2nd method of displaying is using foreach
+            foreach (Product p in inventory)
+                Console.WriteLine(p);
+
+            //Calculate the inventory value
+            decimal total = 0;
+            foreach (Product p in inventory)
+                total += p.InventoryValue();
+            Console.WriteLine("\nTotal inventory: " + total.ToString("c"));
+
+            Console.WriteLine("\n\nPress any key to finish");
+            Console.ReadKey();
+        }
+    }
+}
