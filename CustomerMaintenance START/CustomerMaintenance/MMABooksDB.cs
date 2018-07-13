85044b3fe87297445f9aee54c4abd85403c460ea
dinosaurbytes <37344044+dinosaurbytes@users.noreply.github.com>
2018-07-13 08:21:38 -0600

    validation

---
+++ CustomerMaintenance START/CustomerMaintenance/MMABooksDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient; //ADO.NET data provider for Sql Server
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerMaintenance
+{
+    public static class MMABooksDB
+    {
+        public static SqlConnection GetConnection()     //prepares the connection object 
+        {
+            string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=MMABooks;Integrated Security=True";
+            SqlConnection con = new SqlConnection(connectionString);        //Connection string object
+            return con;             // return the object 
+        }
+    }
+}
---
+++ CustomerMaintenance START/CustomerMaintenance/StateDB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerMaintenance
+{
+    public static class StateDB
+    {
+        public static List<State> GetStates()      //state list defined called GetStates()
+        {
+            List<State> states = new List<State>();     //empty
+
+            //Step 1 Connection
+            SqlConnection connection = MMABooksDB.GetConnection();          // getting connection form the MMABooksDB class
+
+            //Step 2 Command
+            string selectStatement = "SELECT StateCode, StateName FROM States ORDER BY StateName";  //SQL command
+            SqlCommand cmd = new SqlCommand(selectStatement, connection);   //Takes two parameters, the stament and the connection
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                State s;
+                while (reader.Read())       //while there is another record
+                {
+                    s = new State();      //new state object
+                    s.StateCode = reader["StateCode"].ToString();
+                    s.StateName = reader["StateName"].ToString();
+                    states.Add(s);
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return states;
+
+            
+        }
+    }
+}
