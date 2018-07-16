de33f013f688d2c38f4fb4a2486d4cebd1086c67
dinosaurbytes <37344044+dinosaurbytes@users.noreply.github.com>
2018-07-16 11:49:40 -0600

    Module10

--- EntityFrameworkDemo/EntityFrameworkDemo/Form1.Designer.cs
+++ EntityFrameworkDemo/EntityFrameworkDemo/Form1.Designer.cs
@@ -36,12 +36,14 @@
             System.Windows.Forms.Label stateLabel;
             System.Windows.Forms.Label zipCodeLabel;
             this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.cityTextBox = new System.Windows.Forms.TextBox();
             this.customerIDComboBox = new System.Windows.Forms.ComboBox();
+            this.customerBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.customerBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.nameTextBox = new System.Windows.Forms.TextBox();
             this.stateTextBox = new System.Windows.Forms.TextBox();
             this.zipCodeTextBox = new System.Windows.Forms.TextBox();
-            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.invoicesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.invoicesDataGridView = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -53,6 +55,8 @@
             stateLabel = new System.Windows.Forms.Label();
             zipCodeLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.invoicesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.invoicesDataGridView)).BeginInit();
             this.SuspendLayout();
@@ -66,14 +70,6 @@
             addressLabel.TabIndex = 1;
             addressLabel.Text = "Address:";
             // 
-            // addressTextBox
-            // 
-            this.addressTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "Address", true));
-            this.addressTextBox.Location = new System.Drawing.Point(104, 85);
-            this.addressTextBox.Name = "addressTextBox";
-            this.addressTextBox.Size = new System.Drawing.Size(121, 20);
-            this.addressTextBox.TabIndex = 2;
-            // 
             // cityLabel
             // 
             cityLabel.AutoSize = true;
@@ -82,14 +78,6 @@
             cityLabel.Size = new System.Drawing.Size(27, 13);
             cityLabel.TabIndex = 3;
             cityLabel.Text = "City:";
-            // 
-            // cityTextBox
-            // 
-            this.cityTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "City", true));
-            this.cityTextBox.Location = new System.Drawing.Point(104, 111);
-            this.cityTextBox.Name = "cityTextBox";
-            this.cityTextBox.Size = new System.Drawing.Size(121, 20);
-            this.cityTextBox.TabIndex = 4;
             // 
             // customerIDLabel
             // 
@@ -100,15 +88,6 @@
             customerIDLabel.TabIndex = 5;
             customerIDLabel.Text = "Customer ID:";
             // 
-            // customerIDComboBox
-            // 
-            this.customerIDComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "CustomerID", true));
-            this.customerIDComboBox.FormattingEnabled = true;
-            this.customerIDComboBox.Location = new System.Drawing.Point(104, 28);
-            this.customerIDComboBox.Name = "customerIDComboBox";
-            this.customerIDComboBox.Size = new System.Drawing.Size(121, 21);
-            this.customerIDComboBox.TabIndex = 6;
-            // 
             // nameLabel
             // 
             nameLabel.AutoSize = true;
@@ -117,14 +96,6 @@
             nameLabel.Size = new System.Drawing.Size(38, 13);
             nameLabel.TabIndex = 7;
             nameLabel.Text = "Name:";
-            // 
-            // nameTextBox
-            // 
-            this.nameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "Name", true));
-            this.nameTextBox.Location = new System.Drawing.Point(104, 141);
-            this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(121, 20);
-            this.nameTextBox.TabIndex = 8;
             // 
             // stateLabel
             // 
@@ -135,14 +106,6 @@
             stateLabel.TabIndex = 9;
             stateLabel.Text = "State:";
             // 
-            // stateTextBox
-            // 
-            this.stateTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "State", true));
-            this.stateTextBox.Location = new System.Drawing.Point(104, 167);
-            this.stateTextBox.Name = "stateTextBox";
-            this.stateTextBox.Size = new System.Drawing.Size(121, 20);
-            this.stateTextBox.TabIndex = 10;
-            // 
             // zipCodeLabel
             // 
             zipCodeLabel.AutoSize = true;
@@ -152,6 +115,61 @@
             zipCodeLabel.TabIndex = 11;
             zipCodeLabel.Text = "Zip Code:";
             // 
+            // addressTextBox
+            // 
+            this.addressTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "Address", true));
+            this.addressTextBox.Location = new System.Drawing.Point(104, 85);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(121, 20);
+            this.addressTextBox.TabIndex = 2;
+            // 
+            // customerBindingSource
+            // 
+            this.customerBindingSource.DataSource = typeof(EntityFrameworkDemo.Customer);
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "City", true));
+            this.cityTextBox.Location = new System.Drawing.Point(104, 111);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(121, 20);
+            this.cityTextBox.TabIndex = 4;
+            // 
+            // customerIDComboBox
+            // 
+            this.customerIDComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "CustomerID", true));
+            this.customerIDComboBox.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.customerBindingSource, "CustomerID", true));
+            this.customerIDComboBox.FormattingEnabled = true;
+            this.customerIDComboBox.Location = new System.Drawing.Point(104, 28);
+            this.customerIDComboBox.Name = "customerIDComboBox";
+            this.customerIDComboBox.Size = new System.Drawing.Size(121, 21);
+            this.customerIDComboBox.TabIndex = 6;
+            this.customerIDComboBox.SelectedIndexChanged += new System.EventHandler(this.customerIDComboBox_SelectedIndexChanged);
+            // 
+            // customerBindingSource2
+            // 
+            this.customerBindingSource2.DataSource = typeof(EntityFrameworkDemo.Customer);
+            // 
+            // customerBindingSource1
+            // 
+            this.customerBindingSource1.DataSource = typeof(EntityFrameworkDemo.Customer);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "Name", true));
+            this.nameTextBox.Location = new System.Drawing.Point(104, 141);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(121, 20);
+            this.nameTextBox.TabIndex = 8;
+            // 
+            // stateTextBox
+            // 
+            this.stateTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "State", true));
+            this.stateTextBox.Location = new System.Drawing.Point(104, 167);
+            this.stateTextBox.Name = "stateTextBox";
+            this.stateTextBox.Size = new System.Drawing.Size(121, 20);
+            this.stateTextBox.TabIndex = 10;
+            // 
             // zipCodeTextBox
             // 
             this.zipCodeTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerBindingSource, "ZipCode", true));
@@ -159,10 +177,6 @@
             this.zipCodeTextBox.Name = "zipCodeTextBox";
             this.zipCodeTextBox.Size = new System.Drawing.Size(121, 20);
             this.zipCodeTextBox.TabIndex = 12;
-            // 
-            // customerBindingSource
-            // 
-            this.customerBindingSource.DataSource = typeof(EntityFrameworkDemo.Customer);
             // 
             // invoicesBindingSource
             // 
@@ -181,6 +195,7 @@
             this.invoicesDataGridView.Name = "invoicesDataGridView";
             this.invoicesDataGridView.Size = new System.Drawing.Size(250, 220);
             this.invoicesDataGridView.TabIndex = 13;
+            this.invoicesDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.invoicesDataGridView_CellContentClick);
             // 
             // dataGridViewTextBoxColumn1
             // 
@@ -216,7 +231,10 @@
             this.Controls.Add(this.zipCodeTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.invoicesBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.invoicesDataGridView)).EndInit();
             this.ResumeLayout(false);
@@ -237,6 +255,8 @@
         private System.Windows.Forms.DataGridView invoicesDataGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewCheckBoxColumn dataGridViewCheckBoxColumn1;
+        private System.Windows.Forms.BindingSource customerBindingSource1;
+        private System.Windows.Forms.BindingSource customerBindingSource2;
     }
 }
 
--- EntityFrameworkDemo/EntityFrameworkDemo/Form1.cs
+++ EntityFrameworkDemo/EntityFrameworkDemo/Form1.cs
@@ -12,9 +12,35 @@ namespace EntityFrameworkDemo
 {
     public partial class Form1 : Form
     {
+        MMABooksEntities context = new MMABooksEntities();  //object context
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var customers = (from customer in context.Customers.Include("Invoices")
+                             orderby customer.CustomerID
+                             select customer).ToList();     //ToList() executes the LINQ query
+            customerBindingSource.DataSource = customers;
+
+        }
+
+        private void invoicesDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void customerIDComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int customerID = (int)customerIDComboBox.SelectedItem;
+            var customerInvoices = (from invoice in context.Invoices
+                                    where invoice.CustomerID == customerID
+                                    orderby invoice.InvoiceID
+                                    select invoice).ToList();       //list of invoices for selected customer
+            invoicesBindingSource.DataSource = customerInvoices;
         }
     }
 }
